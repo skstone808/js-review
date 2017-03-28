5119452e235effe59abff766423f224de8a603b1
Keola Stone <skstone808@gmail.com>
2017-03-27 20:01:13 -1000

    on a roll

--- exercises.js
+++ exercises.js
@@ -9,15 +9,37 @@ Now, unleash your coding beast!
 /*1.  Variables with a String as their value.
 Declare variables named yourName, neighborName, teacherName.  Console log each variable.*/
 
+var yourName = "Keola";
+console.log(yourName);
+var neighborName = "Brandon";
+console.log(neighborName);
+var teacherName = "Vic";
+console.log(teacherName);
+
 /*2. Variable with a Number as their value.
 Declare variables named numberOfFloorsBurjKhalifa, TokyoOlympicsYear. Please assign the actual data for each variable.
 Console log each variable.*/
 
+var numberOfFloorsBurjKhalifa = 5;
+console.log(numberOfFloorsBurjKhalifa);
+var TokyoOlympicsYear = 1988;
+console.log(TokyoOlympicsYear);
+
 /*3. Variables with Boolean values
 Declare variables named lovesPrepClass, eatsDonuts, drinksGin.  Console log each variable.*/
 
+var lovesPrepClass = true;
+console.log(lovesPrepClass);
+var eatsDonuts = true;
+console.log(eatsDonuts);
+var drinksGin = false;
+console.log(drinksGin);
+
 /*4. Variable with Null values
 Declare variables named completedCodingChallenge, traveledToSpace.  Console log each variable*/
+
+var completedCodingChallenge = null;
+
 
 /*5.  Variables with Array for its value.
 Declare a variable named japanPrefectures and assign the value of any 5 prefectures in Japan.
